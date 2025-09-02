78931997185a6cf49543770922aecb67872f6acf
Md Hamiz Imam <mdhamizimam@gmail.com>
2025-09-02 10:43:08 +0530

    edit

--- src/components/EnrollmentForm.tsx
+++ src/components/EnrollmentForm.tsx
@@ -35,7 +35,7 @@ const EnrollmentForm: React.FC<EnrollmentFormProps> = ({ onBack }) => {
 
     setLoading(true);
     try {
-      const res = await fetch("https://shams-backend-ajrd.onrender.com/api/submit-form", {
+      const res = await fetch("https://shams-backende.onrender.com", {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify(formData),
